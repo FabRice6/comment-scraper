a2c9547428ace274c21db4a04124db1835e68845
Fabrice Verhaert <fabriceverhaert@gmail.com>
2020-10-15 10:12:59 +0200

    initialized video searching script

---
+++ functions/search-vids.py
@@ -0,0 +1,7 @@
+"""
+This script should search and find videos that can then be scraped by comments.py
+
+Returns:
+    String of YouTube URLs with space between.
+    These will be the arguments of the comment scraper comments.py
+"""
