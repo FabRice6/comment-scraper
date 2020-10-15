87c9e4818df99c67711863ae949cb8a8ed0c1457
Fabrice Verhaert <fabriceverhaert@gmail.com>
2020-10-15 10:12:14 +0200

    Main scrip + ftn library created

---
+++ functions/__init__.py
@@ -0,0 +1,7 @@
+"""
+Make Python treat this directory as a module.
+
+The __init__.py file makes Python treat directories containing it as modules. Furthermore, this is the first file to be
+loaded in a module, so you can use it to execute code that you want to run each time a module is loaded, or specify the
+submodules to be exported.
+"""
