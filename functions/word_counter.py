d8b3a8a43ad439acaf583347f6d386f3511296eb
Fabrice Verhaert <fabriceverhaert@gmail.com>
2020-10-15 12:22:05 +0200

    word_counter done

--- functions/word_counter.py
+++ functions/word_counter.py
@@ -2,6 +2,11 @@
 This script will analyze the text by counting occurrences of (groups of) significant words.
 TODO Think about multiplying the comments by #(likes - dislikes) or to focus only on comments with most likes.
 
+Advice:
+    Jos: Ik zou beginnen met 1-grams (geen 2 grams) en de NOT inderdaad als stop woord zien en niet meenemen. 
+    Je zoekt naar thema's waar yogi's over praten? Not is wél belangrijk voor sentiment analyse maar dat doe je dan 
+    beter via classifier op sentence embeddings. Voor topic clustering is NOT niét belangrijk want blijft zelfde topic?
+
 Input: 
     TODO A list of strings representing the comments. Probably better to keep the strings apart for other analyses later
 
@@ -16,15 +21,19 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 def word_counter(list_of_strings, group_size=1, lang='english'):
- 
-    CountVec = CountVectorizer(ngram_range=(group_size, group_size)) #, stop_words=lang)
+
+    # stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its", "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that", "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while", "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before", "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again", "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each", "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than", "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
+
+    CountVec = CountVectorizer(ngram_range=(group_size, group_size), stop_words=lang)
 
     # Transform
     Count_data = CountVec.fit_transform(list_of_strings)
     
     # Create dataframe
-    cv_dataframe=pd.DataFrame(Count_data.toarray(),columns=CountVec.get_feature_names())
-    print(cv_dataframe)
+    df=pd.DataFrame(Count_data.toarray(),columns=CountVec.get_feature_names())
+    df.loc['Totals'] = df.sum()
+    df = pd.concat([df.loc[['Totals']], df[:-1]], axis=0)
+    print(df)
 
 string_1="This is a good job. I will not miss it for anything"
 string_2="This is not good at all"
@@ -34,7 +43,6 @@ strings = [string_1, string_2]
 if __name__ == "__main__":
     word_counter(strings)
     word_counter(strings, 2)
-
 
 # # If you want to run this script to analyze a string passed in the terminal.
 # if __name__ == "__main__":
